c9371be54d1059f771e28005f5bf5729a2f66217
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-07-02 22:23:21 -0700

    Updates for Lita 2.0 compatibility.

--- lib/lita/handlers/totems.rb
+++ lib/lita/handlers/totems.rb
@@ -6,93 +6,102 @@ module Lita
     class Totems < Handler
       class InvalidInvocation < StandardError; end
 
-      route(/^totems\s+add/, to: :add, command: true, help: {
+      route(/^totems\s+add/, :add, command: true, help: {
         "totems add TOTEM" => "Add yourself to the queue for the TOTEM."
       })
-      route(/^totems\s+yield/, to: :yield, command: true, help: {
+      route(/^totems\s+yield/, :yield, command: true, help: {
         "totems yield TOTEM" => "Yield TOTEM to the next user in the queue."
       })
-      route(/^totems\s+kick/, to: :kick, command: true, help: {
+      route(/^totems\s+kick/, :kick, command: true, help: {
         "totems kick TOTEM [USER]" => <<-HELP.chomp
 Forcefully remove either USER or the first user from the queue for TOTEM.
 HELP
       })
       route(
         /^totems\s+create/,
-        to: :create,
+        :create,
         command: true,
         restrict_to: :totem_admins,
         help: { "totems create TOTEM" => "Create a new totem called TOTEM." }
       )
       route(
         /^totems\s+destroy/,
-        to: :destroy,
+        :destroy,
         command: true,
         restrict_to: :totem_admins,
         help: { "totems destroy TOTEM" => "Destroy the TOTEM totem." }
       )
-      route(/^totems/, to: :list, command: true, help: {
+      route(/^totems/, :list, command: true, help: {
         "totems [TOTEM]" => "List the queues for all totems, or only TOTEM."
       })
 
-      def add(matches)
-        validate_format("Format: #{robot.mention_name} totems add TOTEM_NAME")
+      def add(response)
+        validate_format(
+          response,
+          "Format: #{robot.mention_name} totems add TOTEM_NAME"
+        )
 
-        if @totem.add(user)
-          if @totem.holder == user
-            reply "#{user.name} is now in possession of #{@totem}."
+        if @totem.add(response.user)
+          if @totem.holder == response.user
+            response.reply "#{response.user.name} is now in possession of #{@totem}."
           else
-            reply <<-REPLY.chomp
-#{user.name} has been added to the queue for #{@totem}.
+            response.reply <<-REPLY.chomp
+#{response.user.name} has been added to the queue for #{@totem}.
 REPLY
           end
         else
-          reply "#{user.name} is already queued for #{@totem}."
+          response.reply "#{response.user.name} is already queued for #{@totem}."
         end
       rescue InvalidInvocation
       end
 
-      def yield(matches)
-        validate_format("Format: #{robot.mention_name} totems yield TOTEM_NAME")
+      def yield(response)
+        validate_format(
+          response,
+          "Format: #{robot.mention_name} totems yield TOTEM_NAME"
+        )
 
-        if @totem.yield(user)
-          reply "#{user.name} has yielded #{@totem}."
+        if @totem.yield(response.user)
+          response.reply "#{response.user.name} has yielded #{@totem}."
           holder = @totem.holder
           notify(holder, "You are now in possession of #{@totem}.") if holder
         else
-          reply "#{user.name} is not queued for #{@totem}."
+          response.reply "#{response.user.name} is not queued for #{@totem}."
         end
       rescue InvalidInvocation
       end
 
-      def kick(matches)
+      def kick(response)
         validate_format(
+          response,
           "Format: #{robot.mention_name} totems kick TOTEM_NAME [USER]"
         )
 
-        user_name = args[2]
+        user_name = response.args[2]
         kick_user = user_name && User.find_by_name(user_name)
         if user_name && !kick_user
-          reply "There is no such user as #{user_name}"
+          response.reply "There is no such user as #{user_name}"
           return
         end
         initial_holder = @totem.holder
         kick_user = @totem.kick(kick_user)
-        reply "#{kick_user.name} was kicked from the queue for #{@totem}."
+        response.reply <<-REPLY
+#{kick_user.name} was kicked from the queue for #{@totem}.
+REPLY
         notify(kick_user, "You've been kicked out of the queue for #{@totem}.")
         holder = @totem.holder
         if holder && holder != initial_holder
           notify(holder, "You are now in possession of #{@totem}.")
         end
       rescue Totem::EmptyQueue
-        reply "#{@totem} is already empty."
+        response.reply "#{@totem} is already empty."
       rescue Totem::UserNotQueued
-        reply "#{user.name} is not queued for #{@totem}."
+        response.reply "#{response.user.name} is not queued for #{@totem}."
       rescue InvalidInvocation
       end
 
-      def list(matches)
-        totems = determine_totems(args[0])
+      def list(response)
+        totems = determine_totems(response.args[0])
         return unless totems
         output = []
 
@@ -108,38 +117,40 @@ REPLY
         end
 
         if output.empty?
-          reply "There are no totems yet."
+          response.reply "There are no totems yet."
         else
-          reply output.join("\n")
+          response.reply output.join("\n")
         end
       end
 
-      def create(matches)
+      def create(response)
         validate_format(
+          response,
           "Format: #{robot.mention_name} totems create TOTEM_NAME",
           false
         )
 
         if respond_to?(@totem.name)
-          reply "Can't create a totem with the name of a subcommand."
+          response.reply "Can't create a totem with the name of a subcommand."
           return
         end
 
         if @totem.create
-          reply "Created totem #{@totem}."
+          response.reply "Created totem #{@totem}."
         else
-          reply "Totem #{@totem} already exists."
+          response.reply "Totem #{@totem} already exists."
         end
       rescue InvalidInvocation
       end
 
-      def destroy(matches)
+      def destroy(response)
         validate_format(
+          response,
           "Format: #{robot.mention_name} totems destroy TOTEM_NAME"
         )
 
         @totem.destroy
-        reply "Destroyed totem #{@totem}."
+        response.reply "Destroyed totem #{@totem}."
       rescue InvalidInvocation
       end
 
@@ -163,15 +174,15 @@ REPLY
         robot.send_message(source, body)
       end
 
-      def validate_format(format, totem_must_exist = true)
-        @totem = Totem.new(redis, args[1])
+      def validate_format(response, format, totem_must_exist = true)
+        @totem = Totem.new(redis, response.args[1])
 
         if totem_must_exist && !@totem.persisted?
-          reply "There is no totem named #{@totem}."
+          response.reply "There is no totem named #{@totem}."
           raise InvalidInvocation
         end
       rescue Totem::TotemNameRequired
-        reply format
+        response.reply format
         raise InvalidInvocation
       end
     end
